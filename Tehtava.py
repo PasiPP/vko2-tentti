9e5a7ad304499bca25b131c576098d65ccc04576
Viskaali <pasi.poisuo@gmail.com>
2020-07-10 15:22:58 +0300

    päivitys

--- Tehtava.py
+++ Tehtava.py
@@ -1,16 +1,13 @@
-from pprint import pprint
-
 import requests
 import json
 
 
 response = requests.get('https://2ri98gd9i4.execute-api.us-east-1.amazonaws.com/dev/academy-checkpoint2-json')
 data = response.json()
-pprint(data)
-data['items'][0]['parameter']
 
 for items in data['items']:
-    print(f'{items["parameter"]} "\n". {items["number"]}. "\n"')
+    print(f'{items["parameter"]}.')
 
-with open('checkpoint.txt', 'w') as json_file:
-    json.dump(data, json_file)
+with open("checkpoint.txt", "w") as json_file:
+    json.dump(data, json_file, indent=4)
+    json_file.write("\n")
